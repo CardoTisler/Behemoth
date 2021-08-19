5d011efcaac88a54d5b8929704dc8314a0d145fe
Cardo <cardotisler@live.com>
2021-08-19 19:51:26 +0300

    Added CategoryForm and some initial state manip functionality

--- src/components/categories/CategoryForm.js
+++ src/components/categories/CategoryForm.js
@@ -1,53 +1,101 @@
 import { Box, TextField, Button } from '@material-ui/core'
 import { useState } from 'react'
 import { makeStyles } from '@material-ui/core'
-//TODO: Add handleSubmit method that passes on category name and 
-//if isIncomeCategory then pass set
-//handleSubmit should take its data from the state in this component, while the TextField and
-//radio button should manipulate that state
 
-//TODO: Add new TextField to form that only shows up if isIncomeCategory is false.
-//TODO: Add validation to the new TextField - should only accept numbers
-//TODO: Add new hook for categoryBudgetValue
+//TODO: add input validation method to handleSubmit, handleSubmit should not continue if validation
+//method returns false. The validation method should accept the budget value and make sure it is
+//parseable to integer
+
+//TODO: Form UI changes - UI can be finalized.
 
 const useStyles = makeStyles({
     root: {
         margin: '0.5%',
         padding: '1%',
         border: 'solid',
-        borderWidth: '1px'
+        borderWidth: '1px',
+        height: '8rem'
+    }, errorText: {
+        color: 'red',
+        fontSize: '0.8rem'
     }
 })
 
 const CategoryForm = () => {
     const [isIncomeCategory, setIsIncomeCategory] = useState(true)
     const [categoryName, setCategoryName] = useState("")
+    const [categoryBudget, setCategoryBudget] = useState('')
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
     const classes = useStyles()
+
+    //toggle between Income and Expenses categories
+    const handleCategoryChange = () => {
+        setIsIncomeCategory(!isIncomeCategory)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(categoryName)
         
+        //clear state value after sending data
         setCategoryName("")
+        setCategoryBudget("")
     }
 
+    
     const handleInput = (e) => {
-        setCategoryName(e.target.value)
+        //dynamic state update based on input in textfield
+        if(e.target.name === 'categoryNameField'){
+            setCategoryName(e.target.value)
+        } else {
+            //this regex 
+            if(/^\d+$/.test(e.target.value) || e.target.value === ""){
+                setCategoryBudget(e.target.value)
+                if(showErrorMessage) {setShowErrorMessage(false)}
+            } else {
+                if(e.target.value !== ""){
+                    setShowErrorMessage(true)
+                }
+                setCategoryBudget(e.target.value)
+            }
+        }
     }
+
     return (
         <Box>
             <form onSubmit={handleSubmit} className={classes.root}>
                 {/* Input for category name */}
-                <TextField 
-                id='filled-basic' 
-                label='Standard'
-                value={categoryName}
-                onChange={handleInput}/>
 
-                <Button 
-                type="submit"
-                variant="contained"
-                color="primary"> btn</Button>
+                <div>
+                    <TextField 
+                    id='filled-basic' 
+                    label='Category Name'
+                    name='categoryNameField'
+                    value={categoryName}
+                    onChange={handleInput}/>
+
+                    <Button 
+                    type="submit"
+                    variant="contained"
+                    color="primary">Add</Button>
+
+                    <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={handleCategoryChange}>
+                        {isIncomeCategory ? 'Income' : 'Expenses'}
+                    </Button>
+                </div>
+                {!isIncomeCategory &&
+                <div>
+                    <TextField 
+                    id='filled-basic' 
+                    label='Monthly Budget'
+                    name='budgetValueField'
+                    value={categoryBudget}
+                    onChange={handleInput}/>
+
+                    {showErrorMessage && <p className={classes.errorText}>Invalid input!</p>}
+                </div>}
 
             </form>
         </Box>
