c7fd7db592e565f4014dbcde8bd20f20f82cca6b
Cardo Tisler <cardotisler@live.com>
2021-09-10 00:26:33 +0300

    Remove files that shouldve been ignored by git

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+//TODO: Database connectivity test
+//TODO: Transaction add test
+//TODO: Transaction category change test
+//TODO: Transaction delete test
+//TODO: Category add test
+//TODO: Category delete test
